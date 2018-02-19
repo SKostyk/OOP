6d02b88349977d04015b68f2892060e099d6f00d
SKostyk <Svyatoslav.kostyk@mail.com>
2018-02-19 15:54:09 +0200

    labchik

--- Labchik/src/Main.java
+++ Labchik/src/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[]args){
         Scanner scan = new Scanner(System.in);
         System.out.println("Ціна за кілометр становить 3 гривні");
-        System.out.println("Введіть відстань в км ");
         Variable var = new Variable();
-        var.distance = scan.nextDouble();
-        System.out.println("До оплати " + var.distance*var.price1()+"гривень");
+        var.showInfo();
+
+
     }
 }
--- Labchik/src/Variable.java
+++ Labchik/src/Variable.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
 
 public class Variable {
     public double price1(){
         return price;
     }
-    private static double price = 3;
-    double distance;
+    private double price = 3;
+    private double distance;
+
+    void showInfo(){
+        System.out.println("Введіть відстань в км ");
+        Scanner scanner = new Scanner(System.in);
+        distance = scanner.nextDouble();
+        System.out.println("До оплати " + distance*price1()+"гривень");
+    }
 }
